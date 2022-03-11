a4d4b14d77110a93ea8fbf066022e34ab4124e27
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-11 10:53:08 -0800

    code

---
+++ index.js
@@ -0,0 +1,44 @@
+// const person = {
+//   name: 'John Doe',
+//   age: 40,
+//   email: 'johndoe@gmail.com',
+//   child: {
+//     name: 'Jack Doe',
+//     age: 18,
+//     email: 'jackdoe@gmail.com'
+//   }
+// }
+
+// const personName = person.name
+// const age = person.age
+// const email = person.email
+
+// const { name: personName, email, age } = person
+// const { name } = person
+// console.log(name)
+
+// console.log(personName, age, email)
+
+// const { child: { email: childEmail } } = person
+
+// console.log(childEmail)
+
+// const names = ['John Doe', 'Jane Doe', 'Jack Doe', 'Jenny Doe']
+
+// const name1 = names[0]
+// const name2 = names[1]
+// const name3 = names[2]
+// const name4 = names[3]
+
+// const [ hotdog, jellyfish, foobar, computer ] = names
+// const [ , , foobar, computer ] = names
+
+// console.log(name1, name2, name3, name4)
+// console.log(hotdog, jellyfish, foobar, computer)
+
+// const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+// const [ , , , , , , , , , , , x, ...rest] = nums
+
+// console.log(x)
+// console.log(rest)
